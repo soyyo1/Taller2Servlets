5a123914b93f8f59925ed732391422e0a2902882
vale terminadob <nicolas.np605@gmail.com>
2022-04-08 07:04:08 -0500

    arreglo al subir

--- src/main/java/co/edu/unbosque/tiendadeartevale/UserService/SingUpServlet.java
+++ src/main/java/co/edu/unbosque/tiendadeartevale/UserService/SingUpServlet.java
@@ -1,2 +1,30 @@
-package co.edu.unbosque.tiendadeartevale.UserService;public class SingUpServlet {
-}
+package co.edu.unbosque.tiendadeartevale.UserService;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.File;
+import java.io.IOException;
+
+    @WebServlet(name = "signup", value = "/signup")
+    public class SingUpServlet extends HttpServlet {
+
+        public void init() {}
+
+        public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+
+            try {
+                new UserService().createUser(username, password, getServletContext().getRealPath("") + File.separator);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            response.sendRedirect("./index.html");
+        }
+
+        public void destroy() {}
+    }
--- src/main/java/co/edu/unbosque/tiendadeartevale/UserService/UserService.java
+++ src/main/java/co/edu/unbosque/tiendadeartevale/UserService/UserService.java
@@ -1,2 +1,41 @@
-package co.edu.unbosque.tiendadeartevale.UserService;public class UserService {
+package co.edu.unbosque.tiendadeartevale.UserService;
+import co.edu.unbosque.tiendadeartevale.DTOS.User;
+import com.opencsv.bean.*   ;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class UserService {
+
+    public List<User> getUsers() throws IOException {
+
+        List<User> users;
+
+        try (InputStream is = UserService.class.getClassLoader()
+                .getResourceAsStream("users.csv")) {
+
+            HeaderColumnNameMappingStrategy<User> strategy = new HeaderColumnNameMappingStrategy<>();
+            strategy.setType(User.class);
+
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))) {
+
+                CsvToBean<User> csvToBean = new CsvToBeanBuilder<User>(br)
+                        .withType(User.class)
+                        .withMappingStrategy(strategy)
+                        .withIgnoreLeadingWhiteSpace(true)
+                        .build();
+
+                users = csvToBean.parse();
+            }
+        }
+
+        return users;
+    }
+
+    public void createUser(String username, String password, String path) throws IOException {
+        String newLine = "\n" + username + "," + password + ",customer";
+        FileOutputStream os = new FileOutputStream(path + "WEB-INF/classes/" + "users.csv", true);
+        os.write(newLine.getBytes());
+        os.close();
+    }
 }
